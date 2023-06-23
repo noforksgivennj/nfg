260619ddba6c842dc3759c4fc7578a4dc14db27e
Bryce Robinson <74065233+codebybryce@users.noreply.github.com>
2023-06-23 17:41:03 -0400

    Update IG Icon

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import {useState, useEffect, useCallback} from 'react'
 import Main from "../components/layouts/main/Main";
 import LogoNFG from '../components/Logo/LogoNFG';
+import { GrInstagram } from 'react-icons/gr'
 
 import styled from '../styles/Home.module.css'
 
@@ -8,29 +9,33 @@ import styled from '../styles/Home.module.css'
 // import default react-pdf entry
 import { Document, Page, pdfjs } from "react-pdf";
 // import pdf worker as a url, see `next.config.js` and `pdf-worker.js`
-import workerSrc from "../pdf-worker"
+import { IconContext } from "react-icons";
 
-pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
 
 export default function Home() {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
+  
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
   return (
+    <IconContext.Provider value={{ size:"3rem" }}>
+
+    
     <Main>
       <div className={styled.home}>
         <div className={styled.tempHero}>
         <LogoNFG width={350}/>
         <div>
-         <button onClick={()=>{window.location.href = 'https://www.instagram.com/noforksgivennj/'}}>Check Us Out</button>
+         <button onClick={()=>{window.location.href = 'https://www.instagram.com/noforksgivennj/'}}><GrInstagram /></button>
         </div>
 
         </div>
       </div>
 
     </Main>
+    </IconContext.Provider>
   )
   }
